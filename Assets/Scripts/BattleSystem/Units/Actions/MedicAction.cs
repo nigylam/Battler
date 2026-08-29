0b52c75a9f9680dfce93347fcbb3d07261664544
nigylam <malygin.vadim2010@yandex.ru>
2026-08-29 14:36:31 +0300

    Medic action

--- Assets/Scripts/BattleSystem/Units/Actions/MedicAction.cs
+++ Assets/Scripts/BattleSystem/Units/Actions/MedicAction.cs
@@ -7,17 +7,10 @@ namespace Battler.BattleSystem.Units.Actions
 {
     public class MedicAction : UnitAction
     {
-        [SerializeField] private int _healPerTime;
-        [SerializeField] private int _healPerTimeUpgraded;
         [SerializeField] private MedicVisual _visual;
         [SerializeField] private Sound _sound;
 
         public override bool IsSingleAction => true;
-
-        public override void Upgrade()
-        {
-            _healPerTime = _healPerTimeUpgraded;
-        }
 
         protected override void Disable()
         {
@@ -38,7 +31,7 @@ namespace Battler.BattleSystem.Units.Actions
 
         private void Heal(Unit target)
         {
-            target.Heal(_healPerTime);
+            target.Heal(CurrentActionValue);
         }
     }
 }
